2f00d46ff6dfed8d7c94779f7a9b971814f0ee16
MeCandyCat <mecandycat@gmail.com>
2024-05-25 20:47:19 +0530

    Next UI

---
+++ app/components/nav.jsx
@@ -0,0 +1,47 @@
+import {
+    Navbar, 
+    NavbarBrand, 
+    NavbarContent, 
+    NavbarItem, 
+  } from "@nextui-org/navbar";
+import {Button} from '@nextui-org/button'; 
+import {Link} from "@nextui-org/link";
+
+const NavigationBar = () => {
+    return (
+        <Navbar shouldHideOnScroll>
+        <NavbarBrand>
+          <p className="font-bold text-inherit">PrimeQuiz</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem isActive>
+            <Link aria-current="page">
+              Home
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#" >
+              Post
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              About
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem className="hidden lg:flex">
+            <Link href="#">Login</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Button as={Link} color="primary" href="#" variant="flat">
+              Sign Up
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    );
+  };
+  
+  export default NavigationBar;
